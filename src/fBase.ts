4579edcf9179ecedd388a72fa860d6202cd9a4fa
singyuKang <rkdtlscks123@naver.com>
2022-11-08 11:22:58 +0900

    Update Enviroment Fbase.ts

--- src/fBase.ts
+++ src/fBase.ts
@@ -12,13 +12,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBf7bG3Rr6yYXwBckOOEQNA4vXBqWDWz2k",
-  authDomain: "react-masterclass-4bfc4.firebaseapp.com",
-  projectId: "react-masterclass-4bfc4",
-  storageBucket: "react-masterclass-4bfc4.appspot.com",
-  messagingSenderId: "270912741538",
-  appId: "1:270912741538:web:e7a831e3d6c1a24b2737fb",
-  measurementId: "G-8FB2J08ENH",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
